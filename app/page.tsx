60608e9aae8e2bdf21850bd6260d8bf243cf7afa
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-01-16 14:39:03 -0600

    added all most product images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import gencraft from "../public/gencraft.png";
 import { usePathname } from "next/navigation";
+import Cards from "./ui/home/cards";
+
+const categories = [
+  {
+    id: 1,
+    name: "Shirts",
+    image: "bg-shirt-image",
+    link: "/products/shirts",
+  },
+  {
+    id: 2,
+    name: "Hats",
+    image: "bg-hat-image",
+    link: "/products/hats",
+  },
+  {
+    id: 3,
+    name: "Jackets",
+    image: "bg-jacket-image",
+    link: "/products/jackets",
+  },
+];
 
 export default function Home() {
   const pathname = usePathname();
@@ -27,23 +47,20 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+
       <div>
-        <h1>Shirts</h1>
-        <Link href="/products/shirts">
-          <Image src={gencraft} alt="Sad Coding" width={500} height={500} />
-        </Link>
-      </div>
-      <div>
-        <h1>Hats</h1>
-        <Link href="/products/hats">
-          <Image src={gencraft} alt="Sad Coding" width={500} height={500} />
-        </Link>
-      </div>
-      <div>
-        <h1>Jackets</h1>
-        <Link href="/products/jackets">
-          <Image src={gencraft} alt="Sad Coding" width={500} height={500} />
-        </Link>
+        {categories.map((category) => (
+          <div
+            key={category.id}
+            className="bg-cover w-[31rem] h-[20rem] flex justify-center items-center"
+          >
+            <Cards
+              backgroundUrl={category.image}
+              categoryName={category.name}
+              imageLink={category.link}
+            />
+          </div>
+        ))}
       </div>
     </main>
   );
