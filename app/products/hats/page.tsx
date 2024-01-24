89b0c5c95cb23af82e82094efa176ee18169734d
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-01-23 18:26:35 -0600

    test vercel error not db url

--- app/products/hats/page.tsx
+++ app/products/hats/page.tsx
@@ -1,45 +1,9 @@
 import ProductMap from "../../ui/products/productMap";
+import { fetchAllProductHat } from "@/app/lib/data";
 
-const hats = [
-  {
-    id: 1,
-    price: 100,
-    name: "Hat 1",
-    description: "Hat 1 description",
-    image: "/img/black-hat.png",
-  },
-  {
-    id: 2,
-    price: 200,
-    name: "Hat 2",
-    description: "Hat 2 description",
-    image: "/img/la-hat.png",
-  },
-  {
-    id: 3,
-    price: 300,
-    name: "Hat 3",
-    description: "Hat 3 description",
-    image: "/img/sun-hat.png",
-  },
-  {
-    id: 4,
-    price: 400,
-    name: "Hat 4",
-    description: "Hat 4 description",
-    image: "/img/nike-hat.png",
-  },
-  {
-    id: 5,
-    price: 500,
-    name: "Hat 5",
-    description: "Hat 5 description",
-    image: "/img/fedora-straw.png",
-  }
+export default async function Page() {
+  const hats = await fetchAllProductHat();
 
-];
-
-export default function Page() {
   return (
     <>
       <div>
