9d0913572194aad5a47002d730b988b7d02d8af1
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-02-07 23:10:02 -0600

    navbar now has cart and user session together

--- app/ui/navbar/navBar.tsx
+++ app/ui/navbar/navBar.tsx
@@ -1,25 +1,14 @@
-// "use client";
-
 import Link from "next/link";
-import cartSvg from "../../../public/svg/cart.svg";
-import barsSvg from "../../../public/svg/bars.svg";
-import Image from "next/image";
-import Cart from "../cart/cart";
-import { useNavigation } from "@/app/utils/openCart";
-import WelcomeUser from "./nav-form";
-import { logout } from "@/app/lib/actions";
-
+import { Cart } from "@/app/ui/cart/cart";
+import UserSession from "@/app/utils/userSession";
+// import { BarsImage } from "@/app/utils/openCart";
 
 export default function NavBar() {
-  // const { isCartOpen, handleCartClick } = useNavigation();
-
-
   return (
     <nav>
       <div className="flex justify-end gap-10">
         <ul className="flex flex-grow justify-end items-center gap-10">
           <li>
-            {" "}
             <Link href="/">Home</Link>
           </li>
           <li>
@@ -28,34 +17,11 @@ export default function NavBar() {
           <li>
             <Link href="/login">Login</Link>
           </li>
-          <WelcomeUser />
-
+          <UserSession />
+          <Cart />
+          {/* <BarsImage /> */}
         </ul>
-        {/* <Image
-          src={barsSvg}
-          alt="bars"
-          width={50}
-          height={50}
-          className="pt-5"
-        /> */}
-        {/* <Image
-          src={cartSvg}
-          alt="cart"
-          width={100}
-          height={50}
-          className="pr-4 pt-5 cursor-pointer"
-          // onClick={handleCartClick}
-
-        /> */}
       </div>
-
-      {/* <div>
-        {isCartOpen && (
-          <div className="border-4 border-black py-7 my-4 absolute right-0 bg-white rounded w-full md:w-1/2 lg:w-1/3 h-1/2">
-            <Cart handleCartClick={handleCartClick}/>
-          </div>
-        )}
-      </div> */}
     </nav>
   );
 }
---
+++ app/utils/userSession.tsx
@@ -0,0 +1,21 @@
+"use server";
+
+import { getSession } from "@/app/lib/actions";
+
+export default async function UserSession() {
+  const session = await getSession();
+
+  if (!session) {
+    // If session is null, render nothing
+    return null;
+  }
+
+  return (
+    <>
+      <div>
+        <p>Welcome {session.user.name}</p>
+        <p>{session.user.email}</p>
+      </div>
+    </>
+  );
+}
