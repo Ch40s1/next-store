5a0b6682188af21d3b009ae09119d2dd6bbab094
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-02-10 13:31:37 -0600

    added simple navbar style

--- app/ui/cart/cart.tsx
+++ app/ui/cart/cart.tsx
@@ -1,28 +1,25 @@
 "use client";
-
 import { useState } from "react";
 import cartSvg from "../../../public/svg/cart.svg";
-import barsSvg from "@/public/svg/bars.svg";
 import Image from "next/image";
 
-
-export function Cart(){
+export function Cart() {
   const [isCartOpen, setCartOpen] = useState(false);
 
   const handleCartClick = () => {
     setCartOpen(!isCartOpen);
   };
+
   return (
     <div>
       <div>
-      <Image
-        src={cartSvg}
-        alt="cart"
-        width={50}
-        height={50}
-        className="pr-4 pt-5 cursor-pointer"
-        onClick={handleCartClick}
-      />
+        <Image
+          src={cartSvg}
+          alt="cart"
+          width={37}
+          className="cursor-pointer"
+          onClick={handleCartClick}
+        />
       </div>
       {isCartOpen && <CartImage isCartOpen={isCartOpen} setCartOpen={setCartOpen} />}
     </div>
@@ -30,78 +27,53 @@ export function Cart(){
 }
 
 export function CartImage({ isCartOpen, setCartOpen }: { isCartOpen: boolean, setCartOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
-
   const handleCartClick = () => {
     setCartOpen(!isCartOpen);
   };
 
-  if (isCartOpen) {
-    return (
-      <>
-        {/* This is going to be start of the cart planning */}
-        {/* First I need to get a login setup
-    this will allow me to then query a user from the session and
-    use its id to query the cart items from the database
-    I will then need to display the items in the cart and allow the user to remove items from the cart
-    I will also need to display the total of the cart
-    I will also need to allow the user to checkout and pay for the items in the cart
-    I will also need to allow the user to add items to the cart from the product page
-    I will also need to allow the user to update the quantity of the items in the cart
-    I will also need to allow the user to add a discount code to the cart
-     in order to ge the user i will need to use the useSession hook from next-auth. This means setting up authentication in the app
-    */}
-        <div className="border-4 border-black py-7 my-4 absolute right-0 top-7 bg-white rounded w-full md:w-1/2 lg:w-1/3 h-1/2">
-          <div className="flex mb-8">
-            <button
-              className="bg-slate-500 w-14 h-14 absolute -top-5 order-2 -left-4 rounded-full"
-              onClick={handleCartClick}
-            >
-              X
-            </button>
-            <h1 className="w-full text-center underline">Cart</h1>
-          </div>
-          <div>
-            <ul className="flex flex-col gap-8">
-              <div className="grid grid-cols-4">
-                <div className="grid grid-cols-subgrid gap-6">
-                  <h3>Item</h3>
-                  <div>Nike cap</div>
-                  <div>LA cap</div>
+  return (
+    <>
+      <div className={`fixed top-0 right-0 h-full w-full bg-black bg-opacity-50 z-50 ${isCartOpen ? '' : 'hidden'}`}>
+        <div className={`transform transition-transform ml-3 ${isCartOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+          <div className="border-4 border-black py-7 my-4 absolute right-1 top-40 bg-white rounded w-full md:w-1/2 lg:w-1/3 h-100">
+            <div className="flex mb-8">
+              <button
+                className="bg-slate-400 w-14 h-14 absolute -top-5 order-2 -left-4 rounded-full"
+                onClick={handleCartClick}
+              >
+                X
+              </button>
+              <h1 className="w-full text-center underline">Cart</h1>
+            </div>
+            <div>
+              <ul className="flex flex-col gap-8">
+                <div className="grid grid-cols-4">
+                  <div className="grid grid-cols-subgrid gap-6">
+                    <h3>Item</h3>
+                    <div>Nike cap</div>
+                    <div>LA cap</div>
+                  </div>
+                  <div className="grid grid-cols-subgrid gap-6">
+                    <h3>Price</h3>
+                    <div>40.99</div>
+                    <div>42.99</div>
+                  </div>
+                  <div className="grid grid-cols-subgrid gap-6">
+                    <h3>Shipping</h3>
+                    <div>19.99</div>
+                    <div>20.99</div>
+                  </div>
+                  <div className="grid grid-cols-subgrid gap-6">
+                    <h3>Total</h3>
+                    <div>60.98</div>
+                    <div>63.98</div>
+                  </div>
                 </div>
-                <div className="grid grid-cols-subgrid gap-6">
-                  <h3>Price</h3>
-                  <div>40.99</div>
-                  <div>42.99</div>
-                </div>
-                <div className="grid grid-cols-subgrid gap-6">
-                  <h3>Shipping</h3>
-                  <div>19.99</div>
-                  <div>20.99</div>
-                </div>
-                <div className="grid grid-cols-subgrid gap-6">
-                  <h3>Total</h3>
-                  <div>60.98</div>
-                  <div>63.98</div>
-                </div>
-              </div>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
-      </>
-    );
-  }
-}
-export function BarsImage() {
-  const [isCartOpen, setCartOpen] = useState(false);
-
-  const handleCartClick = () => {
-    setCartOpen(!isCartOpen);
-  };
-  // ecentually this is going to hold some props that will allow me to open the navbar
-  // and make it repsponsive
-  return (
-    <div>
-      <Image src={barsSvg} alt="bars" width={50} height={50} className="pt-5" />
-    </div>
+      </div>
+    </>
   );
 }
--- app/ui/navbar/navBar.tsx
+++ app/ui/navbar/navBar.tsx
@@ -1,26 +1,13 @@
-import Link from "next/link";
-import { Cart } from "@/app/ui/cart/cart";
 import UserSession from "@/app/utils/userSession";
-// import { BarsImage } from "@/app/utils/openCart";
+import NavBars from "./nav-Responsive";
 
 export default function NavBar() {
   return (
-    <nav>
-      <div className="flex justify-end gap-10">
-        <ul className="flex flex-grow justify-end items-center gap-10">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/products">Shop</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <UserSession />
-          <Cart />
-          {/* <BarsImage /> */}
-        </ul>
+    <nav className="rounded-lg shadow shadow-black">
+      <div>
+        <NavBars />
+        <UserSession />
+        {/* <BarsImage /> */}
       </div>
     </nav>
   );
